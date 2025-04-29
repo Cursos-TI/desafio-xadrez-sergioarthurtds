d1ef9501b0f6ef768220046e2e0093dd7f0a37d2
sergioarthurtds <teixeirasilva737@gmail.com>
2025-04-29 13:37:55 +0000

    Desafio Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     // Movimento da TORRE - 5 casas para a direita, usando FOR
     int movimentoTorre = 5;
     printf("Movimento da Torre:\n");
-    for (int i = 1; i <= movimentoTorre; i++)
-    {
+    for (int i = 1; i <= movimentoTorre; i++) {
         printf("Direita\n");
     }
 
@@ -14,8 +12,7 @@ int main()
     int movimentoBispo = 5;
     int i = 1;
     printf("\nMovimento do Bispo:\n");
-    while (i <= movimentoBispo)
-    {
+    while (i <= movimentoBispo) {
         printf("Cima Direita\n");
         i++;
     }
@@ -24,11 +21,30 @@ int main()
     int movimentoRainha = 8;
     int j = 1;
     printf("\nMovimento da Rainha:\n");
-    do
-    {
+    do {
         printf("Esquerda\n");
         j++;
     } while (j <= movimentoRainha);
+
+    // Movimento do CAVALO - 2 casas para baixo e 1 casa para a esquerda, usando FOR e WHILE (loops aninhados)
+    int movimentosVerticais = 2;
+    int movimentosHorizontais = 1;
+
+    printf("\nMovimento do Cavalo:\n");
+
+    // Loop externo (FOR) para as casas na vertical
+    for (int k = 1; k <= movimentosVerticais; k++) {
+        printf("Baixo\n");
+
+        // Loop interno (WHILE) para a casa na horizontal (executa apenas na última repetição)
+        if (k == movimentosVerticais) {
+            int l = 1;
+            while (l <= movimentosHorizontais) {
+                printf("Esquerda\n");
+                l++;
+            }
+        }
+    }
 
     return 0;
 }
